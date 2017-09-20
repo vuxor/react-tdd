fd8d375a5948a7ea81b8f83ec05da5f8dba9f152
Ivan Radeljic <radeljicivan85@gmail.com>
2017-09-21 01:48:57 +0200

    New describe block in CommentBox

--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -3,16 +3,32 @@ import CommentBox from '../../src/components/comment_box';
 
 describe('CommentBox', () => {
   let component;
+
   beforeEach(() => {
     component = renderComponent(CommentBox);
   });
+
   it('has the correct class', () => {
     expect(component).to.have.class('comment-box');
-  })
+  });
+
   it('has a text area', () => {
     expect(component.find('textarea')).to.exist;
-  })
+  });
+
   it ('has a button', () => {
     expect(component.find('button')).to.exist;
-  })
+  });
+
+  describe('entering some text', () => {
+    beforeEach(() => {
+      component.find('textarea').simulate('change', 'new comment');
+    });
+
+    it ('shows that text in the textarea', () => {
+    });
+
+    it ('when submitted, clears the input', () => {
+    });
+  });
 });
