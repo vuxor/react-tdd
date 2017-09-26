346637c73dcbe6d6f1aa6c65ec3481a1526e907f
Ivan Radeljic <radeljicivan85@gmail.com>
2017-09-26 20:15:55 +0200

    Add CommentList component

---
+++ src/components/comment_list.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class CommentList extends Component {
+  render() {
+    return (
+      <div className="comment-list">
+        comment list
+      </div>
+    )
+  }
+}
