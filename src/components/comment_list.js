6ae6e0976e9b1503c4e51a31cb1b0fd64c93aaba
Ivan Radeljic <radeljicivan85@gmail.com>
2017-10-08 13:07:25 +0200

    Edit comment list component

--- src/components/comment_list.js
+++ src/components/comment_list.js
@@ -1,4 +1,17 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const CommentList = props => <ul className="comment-list"></ul>
-export default CommentList
+const CommentList = props => {
+  const list = props.comments.map(comment => <li key={comment}>{comment}</li>)
+  return (
+    <ul className="comment-list">{list}</ul>
+  );
+}
+
+const mapStateToProps = state => (
+  {
+    comments: state.comments,
+  }
+)
+
+export default connect(mapStateToProps)(CommentList);
