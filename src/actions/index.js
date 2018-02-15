dfb67fedb90a78850cbd41221fc77d38780fa3cb
Ivan Radeljic <radeljicivan85@gmail.com>
2018-02-15 07:28:32 +0100

    Add state to check if user is authenticated

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,15 @@
-import { SAVE_COMMENT } from './types';
+import { SAVE_COMMENT, CHANGE_AUTH } from './types';
 
 export function saveComment(comment) {
   return {
     type: SAVE_COMMENT,
     payload: comment,
+  }
+}
+
+export function authenticate(isLoggedIn) {
+  return {
+    type: CHANGE_AUTH,
+    payload: isLoggedIn
   }
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import commentReducer from './comments';
+import authenticationReducer from './authentication';
 
 const rootReducer = combineReducers({
   comments: commentReducer,
+  authenticated: authenticationReducer,
 });
 
 export default rootReducer;
