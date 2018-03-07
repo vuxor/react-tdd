74f1c3714771833e10fb201872e154174fc59b99
Ivan Radeljic <radeljicivan85@gmail.com>
2018-03-07 08:15:27 +0100

    Add redux-form package

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
+import { reducer as form } from 'redux-form';
 
 import commentReducer from './comments';
 import authenticationReducer from './authentication';
@@ -6,6 +7,7 @@ import authenticationReducer from './authentication';
 const rootReducer = combineReducers({
   comments: commentReducer,
   authenticated: authenticationReducer,
+  form
 });
 
 export default rootReducer;
