93153e318d478336ba17af98244f48cc6d729fde
Ivan Radeljic <radeljicivan85@gmail.com>
2018-03-07 09:00:09 +0100

    Add signin component to routes

--- src/index.js
+++ src/index.js
@@ -7,15 +7,20 @@ import Resources from './components/resources';
 
 import reqAuth from './components/require_authentication';
 import App from './components/app';
+import Signin from './components/auth/signin';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={createStoreWithMiddleware(
+    reducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )}>
     <Router history={browserHistory}>
       <Route path="/" component={App}>
         <Route path="/resources" component={reqAuth(Resources)} />
+        <Route path="/signin" component={Signin} />
       </Route>
     </Router>
   </Provider>
